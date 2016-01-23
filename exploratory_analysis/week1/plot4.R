12b09a631233117df99dbd7953485362387c4326
paulwasit <plat.sebastien@hotmail.fr>
2016-01-23 16:28:35 +0100

    correct lables in  plot4

--- exploratory_analysis/week1/plot4.R
+++ exploratory_analysis/week1/plot4.R
@@ -35,7 +35,7 @@ par(mfrow = c(2,2)) # all plots on one page (2x2)
 plot  (workingData$cleanTime, workingData$Global_active_power, type='l', xlab = '', ylab = 'Global Active Power (kilowatts)')
 
 # 1,2
-plot  (workingData$cleanTime, workingData$Voltage, type='l', xlab = '', ylab = 'Voltage')
+plot  (workingData$cleanTime, workingData$Voltage, type='l', xlab = 'datetime', ylab = 'Voltage')
 
 # 2,1
 plot  (workingData$cleanTime, workingData$Sub_metering_1, type='l', xlab = '', ylab = 'Energy Sub metering')
@@ -45,6 +45,6 @@ lines (workingData$cleanTime, workingData$Sub_metering_3, type='l', col = 'blue'
 legend("topright", legend = c ('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'), col = c ('black', 'red', 'blue'), lty=1) 
 
 # 2,2
-plot  (workingData$cleanTime, workingData$Global_reactive_power, type='l', xlab = '', ylab = 'Global Reactive Power (kilowatts)')
+plot  (workingData$cleanTime, workingData$Global_reactive_power, type='l', xlab = 'datetime', ylab = 'Global_reactive_power')
 
 dev.off() 
